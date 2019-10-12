0274de635836bc472afff01673f5d026b96c2f2c
Rares <raresbabut@gmail.com>
2019-10-12 18:41:19 +0300

    forgot about null part

--- Students/Babut Rares Ovidiu/tema2_nume_prenume/src/tema2_nume_prenume/Tema.java
+++ Students/Babut Rares Ovidiu/tema2_nume_prenume/src/tema2_nume_prenume/Tema.java
@@ -4,10 +4,14 @@ public class Tema {
 	
 
 	public static String name_inventer(String name) {
+		if(name==null)
+			return null;
 		name = name.trim();
 		name = name.replaceAll("\\s+"," ");
 		String[] parts = name.split(" ");
 		String name_final;
+		if(parts.length==1)
+			return name;
 		name_final = parts[parts.length-1]+", "+parts[parts.length-2];
 		for(int i=0;i<parts.length-2;i++)
 			name_final = name_final +" "+parts[i];
@@ -16,7 +20,7 @@ public class Tema {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(name_inventer("         Mr.        Phd.            FirstName         LastName       "));
+		System.out.println(name_inventer("asdasd asd"));
 	}
 
 }
--- Students/Babut Rares Ovidiu/tema2_nume_prenume/src/tema2_nume_prenume/TemaTest.java
+++ Students/Babut Rares Ovidiu/tema2_nume_prenume/src/tema2_nume_prenume/TemaTest.java
@@ -7,12 +7,12 @@ public class TemaTest {
 	
 	@Test
 	public void testName_Inventer () {
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("Mr. Phd. FirstName LastName"));
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("Mr. Phd. FirstName LastName            "));
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("Mr. Phd. FirstName      LastName     "));
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("Mr.         Phd.      FirstName LastName   "));
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("     Mr. Phd. FirstName      LastName   "));
-		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("Mr.       Phd.       FirstName      LastName"));
+		Assert.assertEquals(null,Tema.name_inventer(null));
+		Assert.assertEquals("",Tema.name_inventer(""));
+		Assert.assertEquals("LastName",Tema.name_inventer("LastName"));
+		Assert.assertEquals("LastName",Tema.name_inventer("   LastName   "));
+		Assert.assertEquals("LastName, FirstName",Tema.name_inventer("  FirstName      LastName     "));
+		Assert.assertEquals("LastName, FirstName Mr.",Tema.name_inventer("Mr.           FirstName LastName   "));
 		Assert.assertEquals("LastName, FirstName Mr. Phd.",Tema.name_inventer("        Mr.      Phd.      FirstName      LastName      "));
 		 
 }
