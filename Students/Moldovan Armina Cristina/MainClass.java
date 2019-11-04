6928d231a30de04e19c8ff60802d602aed7d6359
Moldovan Armina <armina.moldovan17@gmail.com>
2019-11-04 21:11:03 +0200

    Third assignment

--- Students/Moldovan Armina Cristina/MainClass.java
+++ Students/Moldovan Armina Cristina/MainClass.java
@@ -1,34 +1,42 @@
-package Tema;
-public class MainClass {
-	public static void main(String[] args)
-	{
-		Account account = new Account(10);//number=10;
-		System.out.println(account.getBalance());
-		account.deposit(58.8);
-		System.out.println(account.getBalance());
-		account.withdraw(23.5);
-		
-		System.out.println( account.getBalance());
-		System.out.println( account.getAccountNumber());
-		System.out.println(account);
-		
-		SavingsAccount savingAccount = new  SavingsAccount(100);
-		savingAccount.addInterest(56.4);
-		System.out.println( savingAccount.getInterest());
-		 
-		CurrentAccount currentAccount = new  CurrentAccount(1000);
-		currentAccount.deposit(1.3);
-	    System.out.println(currentAccount.getBalance());
-	    
-	    currentAccount.withdraw(0.3);
-	    System.out.println(currentAccount.getBalance());
-	    
-	    Bank bank = new Bank();
-	    bank.openAccount(currentAccount, 0);
-	    System.out.println(bank.getAccount(0));
-	    
-	    bank.openAccount(savingAccount, 1);
-	    System.out.println(bank.getAccount(1));
-	  
+import java.util.Iterator;
+
+public class MainClass extends ImplementIterator {
+	public static void main(String[] args) {
+		// paragraf 1
+		ImplementIterator implementIterator = new ImplementIterator();
+		System.out.println("values with for: ");
+		for (String nextValue : implementIterator.getCards()) {
+			System.out.print(nextValue + " ");
+		}
+
+		System.out.println("\nvalues with iterator: ");
+		while (implementIterator.hasNext()) {
+			String nextValue = implementIterator.next();
+			System.out.print(nextValue + " ");
+		}
+
+		// paragraf 2
+		ImplementIterator2 implementIterator2 = new ImplementIterator2();
+
+		Card first = new Card();
+		first.setNumber(1);
+		first.setSuit("a");
+		implementIterator2.put(first);
+
+		Card second = new Card();
+		second.setNumber(2);
+		second.setSuit("b");
+		implementIterator2.put(second);
+
+		Card third = new Card();
+		third.setNumber(1);
+		third.setSuit("a");
+		implementIterator2.put(third);
+
+		System.out.println("\nvalues with iterator: ");
+		while (implementIterator2.hasNext()) {
+			Card card = implementIterator2.next();
+			System.out.print(card);
+		}
 	}
 }
